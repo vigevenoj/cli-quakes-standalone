b0060c727fa089de9d437e21791274d1c5980181
Jacob Vigeveno <vigevenoj@gmail.com>
2016-05-16 20:34:21 -0700

    Whitespace

--- src/main/java/com/sharkbaitextraordinaire/earthquakes/EarthquakeFeedFetcher.java
+++ src/main/java/com/sharkbaitextraordinaire/earthquakes/EarthquakeFeedFetcher.java
@@ -20,50 +20,55 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class EarthquakeFeedFetcher 
 {
-    public static void main( String[] args )
-    {
-    	final String EARTHQUAKE_FEED_URL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
-    	ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    	
-        ClientConfig configuration = new ClientConfig();
-        JerseyClient client =  JerseyClientBuilder.createClient(configuration);
-        JerseyWebTarget target = client.target(EARTHQUAKE_FEED_URL);
-        
-        Invocation.Builder invocationBuilder = target.request();
-        Response response = invocationBuilder.get();
-        
-        System.out.println(response.getStatus());
-        String feedString = response.readEntity(String.class); 
-        
-		try {
-			FeatureCollection fc = mapper.readValue(feedString, FeatureCollection.class);
-			for (Feature feature : fc.getFeatures()) {
-				System.out.println(feature.getProperty("title"));
-				GeoJsonObject g = feature.getGeometry();
-				if (g instanceof Point) {
-					Point p = (Point)g;
-					System.out.println("At " + p.getCoordinates().getLatitude() + ", " + p.getCoordinates().getLongitude());
-				}
-				/* Properties: 
-				* feature.getProperty("place") is a string description of where the earthquake happened
-				* feature.getProperty("time") is the time of the earthquake
-				* feature.getProperty("url") is an html page about the earthquake
-				* feature.getProperty("detail") returns geojson about the earthquake
-				* feature.getProperty("mag") is the magnitude of the earthquake
-				* feature.getProperty("title") is the title of the earthquake event
-				* feature.getGeometry() returns the geometry of the earthquake, latitude, longitude, and depth
-				*/
-			}
-			
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+  public static void main( String[] args )
+  {
+    final String EARTHQUAKE_FEED_URL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+    ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    ClientConfig configuration = new ClientConfig();
+    JerseyClient client =  JerseyClientBuilder.createClient(configuration);
+    JerseyWebTarget target = client.target(EARTHQUAKE_FEED_URL);
+
+    Invocation.Builder invocationBuilder = target.request();
+    Response response = invocationBuilder.get();
+
+    int status = response.getStatus();
+    if (status == 200) {
+
+      String feedString = response.readEntity(String.class); 
+
+      try {
+        FeatureCollection fc = mapper.readValue(feedString, FeatureCollection.class);
+        for (Feature feature : fc.getFeatures()) {
+          System.out.println(feature.getProperty("title"));
+          GeoJsonObject g = feature.getGeometry();
+          if (g instanceof Point) {
+            Point p = (Point)g;
+            System.out.println("At " + p.getCoordinates().getLatitude() + ", " + p.getCoordinates().getLongitude());
+          }
+          /* Properties: 
+           * feature.getProperty("place") is a string description of where the earthquake happened
+           * feature.getProperty("time") is the time of the earthquake
+           * feature.getProperty("url") is an html page about the earthquake
+           * feature.getProperty("detail") returns geojson about the earthquake
+           * feature.getProperty("mag") is the magnitude of the earthquake
+           * feature.getProperty("title") is the title of the earthquake event
+           * feature.getGeometry() returns the geometry of the earthquake, latitude, longitude, and depth
+           */
+        }
+
+      } catch (JsonParseException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (JsonMappingException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    } else {
+      System.out.println("Got " + status + " response instead of 200");
     }
+  }
 }
