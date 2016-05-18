5fb7ccaacfb8c455d2deedb3f3b8eea8efeadf62
Jacob Vigeveno <vigevenoj@gmail.com>
2016-05-17 23:07:00 -0700

    Add mqtt client for location updates

---
+++ src/main/java/com/sharkbaitextraordinaire/earthquakes/EarthquakeMqttClient.java
@@ -0,0 +1,78 @@
+package com.sharkbaitextraordinaire.earthquakes;
+
+import java.util.Properties;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+
+public class EarthquakeMqttClient implements MqttCallback, Runnable {
+	
+	private Properties properties;
+	MqttClient client;
+	MqttConnectOptions connectionOptions;
+	
+	public EarthquakeMqttClient(Properties properties) {
+		this.properties = properties;
+	}
+
+	public void connectionLost(Throwable arg0) {
+		System.out.println("Connection to mqtt broker was lost");
+		
+	}
+
+	public void deliveryComplete(IMqttDeliveryToken arg0) {
+		// No-op, we don't publish
+		
+	}
+
+	public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void run() {
+		String clientID = properties.getProperty("app.client.client_id");
+		String brokerUrl = properties.getProperty("app.broker.url");
+		
+		connectionOptions = new MqttConnectOptions();
+		connectionOptions.setKeepAliveInterval(30);
+		connectionOptions.setUserName(properties.getProperty("app.client.username"));
+		connectionOptions.setPassword(properties.getProperty("app.client.password").toCharArray());
+		
+		Properties sslProperties = new Properties();
+		sslProperties.setProperty("com.ibm.ssl.protocol", properties.getProperty("com.ibm.ssl.protocol"));
+		sslProperties.setProperty("com.ibm.ssl.trustStore", properties.getProperty("com.ibm.ssl.trustStore"));
+		sslProperties.setProperty("com.ibm.ssl.trustStorePassword", properties.getProperty("com.ibm.ssl.trustStorePassword"));
+		
+		connectionOptions.setSSLProperties(sslProperties);
+		
+		try {
+			client = new MqttClient(brokerUrl, clientID);
+			client.setCallback(this);
+			client.connect(connectionOptions);
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Connected to " + brokerUrl);
+		
+		String myTopic = properties.getProperty("app.broker.topic");
+		MqttTopic topic = client.getTopic(myTopic);
+		
+		if (true) {
+			try {
+				int subQoS = 0;
+				client.subscribe(myTopic, subQoS);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
